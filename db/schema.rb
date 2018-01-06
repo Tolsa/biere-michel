27e296d32ee12bd8f8f08621c4a56e8f1b7e877d
Nicolas Amiel <n.amiel.andersen@gmail.com>
2018-01-06 13:01:36 +0100

    adding admin boolean to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180106111757) do
+ActiveRecord::Schema.define(version: 20180106115924) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "beerlists", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -66,6 +69,7 @@ ActiveRecord::Schema.define(version: 20180106111757) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
