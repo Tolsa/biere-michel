ec2021b08fee54c928949dc9bab27eafde5c60c5
Nicolas Amiel <n.amiel.andersen@gmail.com>
2018-01-11 12:37:37 +0100

    fix changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,21 +8,18 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
-
 import 'bootstrap';
 import swal from 'sweetalert';
 
 addPopupToSubmit();
 displayEmails();
-scrollNav ();
-checkEmail ();
-closePopup ();
+scrollNav();
+checkEmail();
+
 
 
 // ------ LISTE DES FUNCTIONS CI DESSOUS //
 
-// === Pour permettre de descendre dans la page
-// en cliquant sur la navbar === //
 function scrollNav () {
   const whoAreWe = document.getElementById('who-button')
   if (whoAreWe){
@@ -52,8 +49,6 @@ function scrollNav () {
   };
 };
 
-// === Contrôle de saisie et parcours
-// utilisateur lors du submit inscription === //
 function addPopupToSubmit () {
   const boutonSubmit = document.getElementById("inscription")
   const guestFirstname = document.getElementById("guest_firstname")
@@ -110,8 +105,6 @@ function addPopupToSubmit () {
   };
 };
 
-// === Pop up avec tous les emails dans un format
-// OK pour copier-coller en cc d'un email === //
 function displayEmails () {
   const mailList = document.querySelector(".export-to-clipboard")
   if (mailList) {
@@ -161,7 +154,6 @@ function displayEmails () {
   };
 };
 
-// === Contrôle de saisie d'email === //
 function checkEmail () {
   const emailTarget = document.getElementById('guest_email')
   if (emailTarget) {
@@ -183,38 +175,7 @@ function checkEmail () {
 };
 
 
-// === Fonctionnement du formulaire de contact === //
-function contactForm () {
-  const contactButton = document.querySelector(".contact-button")
-  if (contactButton) {
-    contactButton.addEventListener("click", (event) => {
-      // swal({
-      //   title: 'Multiple inputs',
-      //   html: true,
-      //   content: 'input',
-      //   focusConfirm: false,
-      //   preConfirm: () => {
-      //     return [
-      //       $('#swal-input1').val(),
-      //       $('#swal-input2').val(),
-      //       $('#swal-input3').val(),
-      //     ]
-      //   }
-      // })
-    });
-  }
 
-}
-
-function closePopup () {
-  const contactPopup = document.getElementById('contact-popup')
-  const closeBtn = document.querySelector('.close-btn')
-  if (closeBtn) {
-    closeBtn.addEventListener("click", (event) => {
-      contactPopup.style.cssText = 'display: none; transition: 1s ease-in;';
-    });
-  }
-}
 
 
 
