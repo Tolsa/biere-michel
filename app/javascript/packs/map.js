4484d8fd65a846f35c99ab5a0581d15f23f15fcd
Nicolas Amiel <n.amiel.andersen@gmail.com>
2018-01-16 10:58:33 +0100

    refining gmaps

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -6,11 +6,12 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const markers = JSON.parse(mapElement.dataset.markers);
   map.addMarkers(markers);
   map.setZoom(19);
+  map.setOptions({ scrollwheel: false });
   if (markers.length === 0) {
     map.setZoom(2);
   } else if (markers.length === 1) {
     map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
+    map.setZoom(15);
   } else {
     map.fitLatLngBounds(markers);
   }
