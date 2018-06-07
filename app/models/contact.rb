539c25b494e0812a3a1b93ae53d128f962177e68
Achille Miklitarian <achillemiklitarian@gmail.com>
2018-06-07 19:10:07 +0200

    need rebase

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,14 @@
 class Contact < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
-  validates :message, presence: true
+
+  # before_create :add_to_list
+
+  # def add_to_list
+  #   list_id = "42337eae19"
+  #   @gb = Gibbon::Request.new
+  #   subscribe = @gb.lists(list_id).members.create(body: { name: self.name, email_address: self.email, status: “subscribed”, double_optin: false })
+  # end
+
 end
+
